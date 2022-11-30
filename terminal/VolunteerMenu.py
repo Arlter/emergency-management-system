1c87a703c483c9838a5bee596c802ba0468d249c
angellam99 <115711996+angellam99@users.noreply.github.com>
2022-11-30 16:27:18 +0000

    Update VolunteerMenu.py

--- terminal/VolunteerMenu.py
+++ terminal/VolunteerMenu.py
@@ -1,3 +1,48 @@
+"""
+Structure:
+
+Volunteer menu:
+[1] Display your camp's information
+    - check input [method 26: raise_error_for_inexistence]
+    - display camp info [method 29: list_emergency_profile]
+
+[2] Manage refugee profile
+    [1] Create refugee profile
+        - check input [method 25: raise_error_for_existence]
+        - create refugee [method 27: create_refugee_profile]
+    [2] Display refugee profile
+        - check input [method 26: raise_error_for_inexistence]
+        - display refugee [method 30: display_emergency_profile] *confusing name
+    [3] Update refugee profile
+        - check input [method 26: raise_error_for_inexistence]
+        - update refugee [method 28: update_refugee_profile]
+
+[3] Messaging system
+    [1] Display admin announcements
+        - check existence [method 26: raise_error_for_inexistence]
+        - display admin message [method 35: vols_display_message] *use corresponding inputs
+    [2] Send message to admin
+        - send message to admin [method 36: vols_send_message] *use corresponding inputs
+    [3] Display messages from your camp
+        - check existence [method 26: raise_error_for_inexistence]
+        - display admin message [method 35: vols_display_message] *use corresponding inputs
+    [4] Send message to your camp
+        - send message to admin [method 36: vols_send_message] *use corresponding inputs
+
+[4] Manage personal profile
+    [1] Display personal profile
+        - display personal profile [method 32: display_personal_profile]
+    [2] Edit personal profile
+        - edit personal profile [method 33: edit_personal_profile]
+
+In every menu:
+[q] Logout
+[b] Back (except in main menu)
+
+***probably don't have to use [method 31: create_personal_profile] and 
+[method 34: availability] in the volunteer menu?
+
+"""
 
 
 class VolunteerMenu:
